393e193d3755f6a3553e99dc6dbc4f951678b0af
marlon23255 <marlonn.rodriguez@hotmail.com>
2024-10-30 06:02:43 +0000

    added the intended part

--- nicks_contribution.py
+++ nicks_contribution.py
@@ -1,2 +1,3 @@
 print("this is nicks contribution to the exercise!")
 print("This is Loren's contribution to the exercise! Following Nick's")
+print("This is Marlon's contribution to the exercise! Following Nick's and Loren's.")
