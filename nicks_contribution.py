7ba50f7aa5064f6634355d6f9081055b1e5dcd84
cat 😽 <cat@wireless-10-104-178-56.umd.edu>
2024-10-30 02:32:26 -0400

    Cat's contribution following everyone else

--- nicks_contribution.py
+++ nicks_contribution.py
@@ -1,3 +1,4 @@
 print("this is nicks contribution to the exercise!")
 print("This is Loren's contribution to the exercise! Following Nick's")
 print("This is Marlon's contribution to the exercise! Following Nick's and Loren's.")
+print("This is Cat's contribution to the exercise! Following everyone else.")
